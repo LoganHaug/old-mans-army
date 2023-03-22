cb7955adbb43ca4b758fe2f7b2627eec8b7892c5
Logan H <logannhaug@gmail.com>
2023-03-21 21:06:06 -0700

    Write the army and soldier class

--- army.js
+++ army.js
@@ -1,18 +1,102 @@
 // Logan Haug
+function randint(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+var j = loadJSON("text.json");
 
 function army() {
     this.soldiers = [];
+    this.dead = [];
+    this.food = randint(5,10);
     this.avg_hp;
     this.avg_attack;
     this.avg_defense;
     this.avg_morale;
+    this.all_names = j["names"];
+    this.equipment_level = 1;
 
-    this.recruit = function() {
-        
-        var sum_morale, sum_hp, sum_attack, sum_defense;
+    this.recruit = function() {    
+        var sum_morale = 0, sum_hp = 0, sum_attack = 0, sum_defense = 0;
+        var names = [];
         for (var soldier in soldiers) {
-            
+            sum_morale += soldier.morale;
+            sum_hp += soldier.hp;
+            sum_attack += soldier.attack;
+            sum_defense += soldier.defense;
+            names.push(soldier.name);
+        }
+        for (var name in all_names) {
+            if (names.includes(name)) {
+                all_names.pop(name);
+            }
+        }
+        var s = new soldier(all_names[randint(0, all_names.length)]);
+        soldiers.push(s);     
+        var l = soldier.length;
+        avg_morale = (sum_morale + s.morale) / l;
+        avg_hp = (sum_hp + s.hp) / l;
+        avg_attack = (sum_attack + s.attack) / l;
+        avg_defense = (sum_defense + s.defense) / l;
+    }
+
+    this.step = function(hp, decay, morale, attack, defense) {
+        if (this.food == 0)
+            decay -= 10;
+        for (var s in this.solders) {
+            s.step(hp, decay, morale, attack, defense);
+        }
+        this.food -= this.soldiers.length;
+    }
+
+    this.equip = function(upgrade_num) {
+        this.equipment_level += upgrade_num;
+        for (var s in this.soldiers) {
+            s.equip(this.equipment_level);
+        }
+    }
+
+    this.hunt = function() {
+        this.food += this.soldiers.length * randint(1,2);
+    }
+
+    this.battle = function(enemy) {
+        var dmg;
+        var defender;
+        while (this.solders.length > 0 && enemy.solders.length > 0) {
+             // This army attacks
+            for (var s in this.soldiers) {
+                dmg = s.attack();
+                if (enemy.soldiers.length > 1) {
+                    defender = enemy.soldiers[randint(0, enemy.solders.length-1)];
+                } else if (enemy.soldiers.length == 1) {
+                    defender = enemy.soldiers[0];
+                } else {
+                    break;
+                }
+                defender.defend(dmg);
+                if (defender.hp == 0) {
+                    enemy.soldiers.pop(defender);
+                }
+            }
+            // enemy army attacks 
+            for (var s in enemy.soldiers) {
+                dmg = s.atatck();
+                if (this.soldiers.length > 1) {
+                    defender = this.soldiers[randint(0, this.soldiers.length-1)];
+                } else if (this.soldiers.length == 1) {
+                    defender = this.soldiers[0];
+                } else {
+                    break;
+                }
+                defender.defend(dmg);
+                if (defender.hp == 0) {
+                    this.soldiers.pop(defender);
+                    this.dead.push(defender);
+                }
+            }
         }
     }
 }
--- soldier.js
+++ soldier.js
@@ -4,22 +4,46 @@ function randint(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function soldier() {
+function soldier(name) {
     this.hp_max = randint(80, 100);
     this.hp = hp_max - randint(0, 15);
     this.morale = randint(50, 100);
-    this.attack = randint(0, 100);
-    this.defense = randint(0, 100);
-    this.state = 1; // 1 = alive, 0 = dead
-    this.name;
-
-    this.die = function() {
-        this.state = 0;
-    }
+    this.attack = randint(0, 10);
+    this.defense = randint(0, 10);
+    this.equipment;
+    this.name = name;
+    this.decay;
 
     this.display = function() {
         var display_str = this.name + " " + this.hp + "/" + this.hp_max + " ";
         display_str += "" + this.attack + " " + this.defense + " " + this.morale;
         return display_str; 
+    }
+
+    this.step = function(hp_step, decay_step, morale_step, attack_step, defense_step) {
+        this.decay += decay_step;
+        this.hp += this.decay;
+        this.morale += morale_step;
+        this.attack += attack_step;
+        this.defense += defense_step;
+    }
+
+    this.attack = function() {
+        return this.attack * this.equipment * (randint(50, 120) / 100);
+    }
+
+    this.defend = function(damage) {
+        if (damage - this.defense < 0) {
+            damage = 0;
+        } else {
+            damage -= defense;
+        }
+        this.hp -= damage;
+        if (this.hp < 0)
+            this.hp = 0;
+    }
+
+    this.equip = function(e) {
+        this.equipment += e;
     }
 }
